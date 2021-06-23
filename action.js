7fcb1c0291e1f67c8a2f4fdb3fb52aaef7a2c1b3
Nikhil Walia <nikhilwalia363@gmail.com>
2021-06-23 07:44:43 +0530

    nav content added

--- action.js
+++ action.js
@@ -10,18 +10,21 @@ function show()
     {
         item.style.display="block";
     }
+    var ele=document.getElementsByClassName("nav-content");
+    ele[0].style.visibility="visible";
 }
-function close()
+function hide()
 {
-    // var ele=document.getElementsByClassName("close");
-    // for(item of ele)
-    // {
-    //     item.style.display="none";
-    // }
-    // var ele=document.getElementsByClassName("open");
-    // for(item of ele)
-    // {
-    //     item.style.display="block";
-    //     console.log(item);
-    // }
+    var ele=document.getElementsByClassName("close");
+    for(item of ele)
+    {
+        item.style.display="none";
+    }
+    var ele=document.getElementsByClassName("open");
+    for(item of ele)
+    {
+        item.style.display="block";
+    }
+    var ele=document.getElementsByClassName("nav-content");
+    ele[0].style.visibility="hidden";
 }
